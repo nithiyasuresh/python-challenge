c8597befd6e32ac8f700a359149006a5b972f030
nithiyasuresh <nithiyasuresh@gmail.com>
2021-05-27 20:17:15 +1000

    Completed pybank analysis code

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -29,16 +29,19 @@ output_path = os.path.join('.', 'Analysis', 'Analysis.txt')
 
 
 # Initialise variables - global variables
-# The total number of months included in the dataset - TotalMonths
+#The total number of months included in the dataset
 TotalMonths = 0
 
-# The net total amount of "Profit/Losses" over the entire period - net_total_amount
+#The net total amount of "Profit/Losses" over the entire period
 net_total_amount = 0
 
-# Calculate the changes in "Profit/Losses" over the entire period - pl_changes
-changes = 0
+#Finding the value of the first row
+value = 0
 
-# avg_change = sum of all changes/no of changes
+#Defining variable for the P&L change
+change = 0
+dates = []
+profits = []
 
 
 # # Method 1: Plain Reading of CSV files
@@ -50,52 +53,52 @@ with open(csvpath) as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    first_month = next(csvreader)
-    # print(first_month)
-
+    # print(f"CSV Header: {csv_header}")
+    first_row = next(csvreader)
     TotalMonths += 1
-
-    net_total_amount += int(first_month[1])
-    previous_month = int(first_month[1])
+    net_total_amount += int(first_row[1])
+    value = int(first_row[1])
 
     # Read each row of data after the header
     for row in csvreader:
-        # print(row)
+        # Keeping track of the dates
+        dates.append(row[0])
 
+        # Calculate the change, then add it to list of changes
+        pl_change = int(row[1]) - value
+        profits.append(pl_change)
+        value = int(row[1])
 
-        # Increase months by 1 to calculate the total no of months
+        #Total number of months
         TotalMonths += 1
 
-        # Calculate net_total_amount of P&L
-        net_total_amount += int(row[1])
+        #Total net amount of "Profit/Losses over entire period"
+        net_total_amount = net_total_amount + int(row[1])
 
-        # Difference between each month
-        # difference = current montsh - last month
-        difference = int(row[1]) - previous_month
-        print(difference)
+        #Greatest increase in profits
+        greatest_increase = max(profits)
+        greatest_index = profits.index(greatest_increase)
+        greatest_date = dates[greatest_index]
 
-        # Add the values in the profit and loss list
-        changes += difference
-        # changes.append(difference)
+        #Greatest decrease (lowest increase) in profits
+        greatest_decrease = min(profits)
+        worst_index = profits.index(greatest_decrease)
+        worst_date = dates[worst_index]
 
-# print(changes)
-# print(len(changes))
-# print(sum(changes))
-average_change = changes/ 85
-
+        #Average change in "Profit/Losses between months over entire period"
+        avg_change = sum(profits) / len(profits)
 
 # Generate output to terminal
 output = (
-    f'Total months: {TotalMonths}\n'
-    f'Total: ${net_total_amount}\n'
-    f'Average Change: {average_change}\n'
-
-
-
-
+    f'Financial Analysis\n'
+    f'---------------------------------\n'
+    f'Total months: {str(TotalMonths)}\n'
+    f'Total: ${str(net_total_amount)}\n'
+    f'Average Change: ${str(avg_change)}\n'
+    f'Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})\n'
+    f'Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})\n'
 )
+
 print(output)
 # save/output to a text file
 # Open the file using "write" mode. Specify the variable to hold the contents
--- PyBank/pybank_nith.py
+++ PyBank/pybank_nith.py
@@ -1,62 +1,63 @@
-# Nithiya's pyBank analysis 
-print("Nithiya")
-
-# Load python modules
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
 import os
-
-# Module for reading CSV files
 import csv
 
+csvpath = os.path.join('.', 'Resources', 'PyBankData.csv')
+# "..\\Resource\\PyBankData.csv"
 
-# # Load csv files from resource folder
-# csvpath = os.path.join('.', 'Resources', 'PyBankData.csv')
-# # "..\\Resource\\PyBankData.csv"
+output_path = os.path.join('.', 'Analysis', 'Analysis.txt')
 
-# output_path = os.path.join('.', 'Analysis', 'Analysis.txt')
+#set path for the file and define variables
+csvpath = os.path.join("resources", "pybank_budget_data.csv")
+total_months = 0
+net_total = 0
+greatest_inc = {"month": "", "value": 0}
+greatest_dec = {"month": "", "value": 0}
+monthly_change = []
+previous_value = 0
+profit_change = 0
 
+#Read csv file, split on commas and account for header
+with open(csvpath) as csvfile: 
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
 
+    for row in csvreader:
+        #profit_loss= int(row[1])
 
-# # # Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     # print(lines)
-#     # print(type(lines))
+        #Total number of months
+        total_months = total_months + 1
 
-# # Initialise variables - global variables
-# # The total number of months included in the dataset - TotalMonths
-# TotalMonths = 0
+        #Net profits (Sum of all profits and loses)
+        net_total = net_total + int(row[1])  
+        
+        #Greatest increase and decrease
+        if greatest_inc["value"] < int(row[1]):
+            greatest_inc["month"] = row[0]
+            greatest_inc["value"] = int(row[1])
+        if greatest_dec["value"] > int(row[1]):
+            greatest_dec["month"] = row[0]
+            greatest_dec["value"] = int(row[1])
 
+         #count average changes for entire period
+        profit_change = int(row[1]) - previous_value
+        monthly_change.append(profit_change)
+        previous_value = int(row[1])
+        
+    #calculate average change
+    average_change = sum(monthly_change) / total_months
 
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${net_total}\n"
+    f"Average Change: ${round(average_change,2)}\n"
+    f"Greatest Increase in Profits: {greatest_inc['month']}  (${greatest_inc['value']})\n"
+    f"Greatest Decrease in Profits: {greatest_dec['month']}  (${greatest_dec['value']})\n"
+)
 
-# # Iterate through csv file and extract information
+#print outputs to terminal
+print(output)
 
-
-# """task is to create a Python script that analyzes the records to calculate each of the following:
-
-# The total number of months included in the dataset
-
-# The net total amount of "Profit/Losses" over the entire period
-
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-# The greatest increase in profits (date and amount) over the entire period
-
-# The greatest decrease in losses (date and amount) over the entire period
-# """
-
-
-# # Generate output to terminal
-# output = (
-#     f'Total number of months is {TotalMonths}'
-
-
-
-
-# )
-# print(output)
-# # save/output to a text file
-# # Open the file using "write" mode. Specify the variable to hold the contents
-# with open(output_path, 'w') as txt_file:
-#     txt_file.write(output)
+with open("analysis/output.txt", "w") as txt_file:
+    txt_file.write(output)
